0f1f819c8724ac6a9df6e4eed22f4b8b90200357
sinhashaan89 <sinhashaan89@gmail.com>
2022-07-25 12:58:40 +0530

    my first commit

---
+++ SDET36L1/MAVEN_PROJECT/src/test/java/org/mavenproject/pratice/PraticeTest1.java
@@ -0,0 +1,41 @@
+package org.mavenproject.pratice;
+
+import org.testng.annotations.Test;
+
+public class PraticeTest1 {
+	
+
+	@Test
+	public void test1() {
+		
+		
+		String browser=System.getProperty("BROWSER");
+		String url=System.getProperty("URL");
+		String un=System.getProperty("USERNAME");
+		String pwd=System.getProperty("PASSWORD");
+		System.out.println("test1 --> PraticeTest1");
+		System.out.println("Browser --> "+browser);
+		System.out.println("URL --> "+url);
+		System.out.println("username --> "+un);
+		System.out.println("password --> "+pwd);
+		System.out.println("Browser --> "+browser);
+		System.out.println("URL --> "+url);
+		System.out.println("username --> "+un);
+		System.out.println("password --> "+pwd);
+		System.out.println("Browser --> "+browser);
+		System.out.println("URL --> "+url);
+		System.out.println("username --> "+un);
+		System.out.println("password --> "+pwd);
+		
+	}
+	
+	@Test
+	public void test2() {
+		
+		
+	}
+	
+	
+	
+
+}
---
+++ SDET36L1/MAVEN_PROJECT/src/test/java/org/mavenproject/pratice/PraticeTest2.java
@@ -0,0 +1,28 @@
+package org.mavenproject.pratice;
+
+import org.testng.annotations.Test;
+
+public class PraticeTest2 {
+	
+
+	@Test
+	public void test1() {
+		
+		System.out.println("test2 --> PraticeTest2");
+		System.out.println("test2 --> PraticeTest");
+	}
+	
+	@Test
+	public void test2() {
+		
+		
+	}
+	
+	@Test
+	public void test3() {
+		
+		System.out.println("test3 --> PraticeTest2");
+	}
+	
+
+}
